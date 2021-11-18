126511080cf4cb98c1d73401c5fb511f843de471
pstuksys <stuksys.paulius@gmail.com>
2021-11-18 15:03:12 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
+import React, {Fragment} from "react";
+import Header from "./components/Layout/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Fragment>
+      <Header/>
+    </Fragment>
   );
 }
 
